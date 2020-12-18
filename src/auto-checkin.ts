786d8bab25151d12e202c145b9a77fe85b54e2d8
William Thoi <willy.thoi@hotmail.com>
2020-12-17 23:44:15 -0500

    Update to stop clicking if full

--- src/auto-checkin.ts
+++ src/auto-checkin.ts
@@ -3,17 +3,46 @@
     const interactiveElement = document.getElementById(
       'submitLoginMission'
     );
-    if (interactiveElement !== null) {
-      console.log("found")
-      interactiveElement.click();
+    
+    const disabled = document.getElementsByClassName(
+      'disabled'
+    );
+
+    const karmaAmount = document.getElementsByClassName(
+      'karma-amount'
+    );
+    let enabled = true;
+
+    for (let i = 0; i < disabled.length; i++) {
+      if (disabled.item(i).id == 'submitLoginMission') {
+        enabled = false;
+        console.log("Check-in button disabled");
+      }
+    }
+
+    console.log(karmaAmount);
+
+    if (enabled && karmaAmount.length != 0) {
+      if (!karmaAmount[0].innerHTML.includes("<span>3000</span>")) {
+        if (interactiveElement !== null) {
+          interactiveElement.click();
+          console.log("Karma points added");
+        }
+      } else {
+        console.log("Karma full");
+      }
     }
   }
 
   function start() {
-    console.log("looking now");
+    console.log("Looking for check-in button");
     clickcheckin();
   }
 
-  start();
+  document.addEventListener("DOMSubtreeModified", function(event){
+    start();
+  });
+
   console.log('WuxiaWorld Auto-checkin');
 })();
+
