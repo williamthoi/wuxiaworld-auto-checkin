050b668e602273e44d6b2ec39c0e9095a72278bf
williamthoi <willy.thoi@hotmail.com>
2023-09-11 19:04:05 -0400

    remove unnecessary bloat

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,35 @@
-var element = document.createElement('script');
-// @ts-ignore
-element.src = chrome.runtime.getURL('dist/src/auto-checkin.js');
-document.body.appendChild(element);
+(function() {
+    function clickButton() {
+  
+      console.log("Looking for check-in button");
+  
+      const buttons: NodeListOf<HTMLButtonElement> = document.querySelectorAll('button:enabled');
+  
+      buttons.forEach((button) => {
+        if (button.textContent === 'COMPLETED' && !button.disabled) {
+          console.log("Completed adding karma");
+          return;
+        }
+  
+        if (button.textContent === 'COMPLETE' && !button.disabled) {
+          button.click();
+          console.log("Karma points added");
+          return;
+        }
+      });
+  
+    }
+  
+    const observer = new MutationObserver(clickButton);
+  
+    // Configure the observer to watch for subtree changes (e.g., dynamic button additions)
+    const observerConfig = { childList: true, subtree: true };
+    
+    // Start observing the DOM for changes
+    observer.observe(document.body, observerConfig);
+    
+    clickButton();
+    console.log('WuxiaWorld Auto-checkin');
+  })();
+  
+  
